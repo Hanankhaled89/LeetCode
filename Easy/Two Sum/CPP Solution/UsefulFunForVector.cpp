d688753c4fdbc2a3982e8d0a0b5915d02f4ecdc9
Hanankhaled89 <hananalgushy2@gmail.com>
2022-12-04 16:08:51 +0200

    VectFunc Commit

--- Easy/Two Sum/CPP Solution/UsefulFunForVector.cpp
+++ Easy/Two Sum/CPP Solution/UsefulFunForVector.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 using std::vector;
+using namespace std;
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+
+
+  void  UsefulFunForVect(vector<int>& nums) {
         
         // Format: vecor<Data Type> name of vector
         // myvector.push_back(value) ==> adds an element to the end of the vector (also Resize it)
@@ -16,18 +17,24 @@ public:
         //myvector.empty() ==>return boolen value if whether vector is empty
         //find(): Used to find the position of element in the vector.  auto it = find(v.begin(), v.end(), K);
         
-    //      vector<int> myvector;
-//         myvector.push_back(3);
-//         myvector.push_back(7);
-//         myvector.push_back(12);
-//         myvector.insert(myvector.begin()+1,0);
-//         myvector.erase(myvector.begin()+3);
-//         cout<< "vector: ";
-//         for (unsigned int  i=0; i<myvector.size();i++)
-//         {
-//          cout<< myvector[i]<<" ";   
-//         }
+         vector<int> myvector;
+        myvector.push_back(3);
+        myvector.push_back(7);
+        myvector.push_back(12);
+        myvector.insert(myvector.begin()+1,0);
+        myvector.erase(myvector.begin()+3);
+        cout<< "vector: ";
+        for (unsigned int  i=0; i<myvector.size();i++)
+        {
+         cout<< myvector[i]<<" ";   
+        }
 
-//  
+
 }
-};
+
+int main()
+{
+     vector<int> nums;
+      UsefulFunForVect(nums);
+    return 0;
+}
