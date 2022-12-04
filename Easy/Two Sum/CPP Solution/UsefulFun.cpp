005021d48ccc98443518b11d1552d452a40db28e
Hanankhaled89 <hananalgushy2@gmail.com>
2022-12-04 15:51:52 +0200

    First Commit

---
+++ Easy/Two Sum/CPP Solution/UsefulFun.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        // Format: vecor<Data Type> name of vector
+        // myvector.push_back(value) ==> adds an element to the end of the vector (also Resize it)
+        //myvector.at(index) ==> Returm element at specific element
+        // myVector.size() ==> return  the sixe of vector
+        //myvector.begin() ==>reads vector from first element (index 0)
+        //myvector.insert(myVector.begin()+integer, new value)==> add elements BEFORE speciefied index number
+        // myvector.erase(myvector.begin()+integer) ==> Remove element At specified index number
+        //myvector.empty() ==>return boolen value if whether vector is empty
+        //find(): Used to find the position of element in the vector.  auto it = find(v.begin(), v.end(), K);
+        
+    //      vector<int> myvector;
+//         myvector.push_back(3);
+//         myvector.push_back(7);
+//         myvector.push_back(12);
+//         myvector.insert(myvector.begin()+1,0);
+//         myvector.erase(myvector.begin()+3);
+//         cout<< "vector: ";
+//         for (unsigned int  i=0; i<myvector.size();i++)
+//         {
+//          cout<< myvector[i]<<" ";   
+//         }
+
+//  
+}
+};
